0d58c26ae51043516fbf6c13aba4a68911feb542
Coulby <nguyenco@oregonstate.edu>
2018-06-10 15:31:22 -0700

    worked on my part for the final project B

--- projects/nguyenco/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
+++ projects/nguyenco/FinalProject/URLValidatorInCorrect/src/test/java/finalprojectB/UrlValidatorTest.java
@@ -1,7 +1,10 @@
 
 package finalprojectB;
-
+import java.lang.StringBuilder;
 import junit.framework.TestCase;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.util.Random;
 
 //You can use this as a skeleton for your 3 different test approach
 //It is an optional to use this file, you can generate your own test file(s) to test the target function!
@@ -16,6 +19,7 @@ public class UrlValidatorTest extends TestCase {
 
    public UrlValidatorTest(String testName) {
       super(testName);
+
    }
 
    
@@ -23,7 +27,7 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 //You can use this function to implement your manual testing	   
-	   
+	UrlValidator myUrlValidator;
    }
    
    
@@ -42,9 +46,72 @@ public class UrlValidatorTest extends TestCase {
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
+	ResultPair[] testUrlScheme = {new ResultPair("http://", true),
+	   				new ResultPair("ftp://", true),
+					new ResultPair("3ht://", false),
+					new ResultPair("http:", false),
+					new ResultPair("://", false)};
+	ResultPair[] testUrlAuthority = {new ResultPair("www.google.com",true),
+					new ResultPair("go.com", true),
+					new ResultPair("256.256.256.256", true),
+					new ResultPair("1.2.3.4.5", false),
+					new ResultPair("aaa", false)};
+	ResultPair[] testUrlPort = {new ResultPair(":80", true),
+	   				new ResultPair(":65535", true),
+					new ResultPair("", true),
+					new ResultPair(":-1", false),
+					new ResultPair(":65a", false)};
+	ResultPair[] testPath = {new ResultPair("/test1", true),
+	   				new ResultPair("/t123", true),
+					new ResultPair("", true),
+					new ResultPair("/..", false),
+					new ResultPair("/..//file", false)};
+	ResultPair[] testUrlQuery = {new ResultPair("?action=view", true),
+	   				new ResultPair("?action=edit&mode=up", true),
+					new ResultPair("", true)};
+
+	//UrlValidator myUrlValidator = new UrlValidator(null, null, 1);
+	//long allowAllSchemes = 1 << 0;
+	//UrlValidator myUrlValidator = new UrlValidator(allowAllSchemes);
+	UrlValidator myUrlValidator = new UrlValidator();
+	Random rand = new Random();
+	for(int i = 0; i < 1000; i++){
+	   StringBuilder testBuffer = new StringBuilder();
+	   boolean expected = true;
+
+	   int testUrlSchemeIdx = rand.nextInt(5);
+	   testBuffer.append(testUrlScheme[testUrlSchemeIdx].item);
+	   expected &= testUrlScheme[testUrlSchemeIdx].valid;
+
+	   int testUrlAuthorityIdx = rand.nextInt(5);
+	   testBuffer.append(testUrlAuthority[testUrlAuthorityIdx].item);
+	   expected &= testUrlAuthority[testUrlAuthorityIdx].valid;
+
+	   int testUrlPortIdx = rand.nextInt(5);
+	   testBuffer.append(testUrlPort[testUrlPortIdx].item);
+	   expected &= testUrlPort[testUrlPortIdx].valid;
+
+	   int testPathIdx = rand.nextInt(5);
+	   testBuffer.append(testPath[testPathIdx].item);
+	   expected &= testPath[testPathIdx].valid;
+
+	   int testUrlQueryIdx = rand.nextInt(3);
+	   testBuffer.append(testUrlQuery[testUrlQueryIdx].item);
+	   expected &= testUrlQuery[testUrlQueryIdx].valid;
+	   
+	   String url = testBuffer.toString();
+	   boolean result = myUrlValidator.isValid(url);
+	
+	   System.out.println(result);
+	   if(result == true){
+	      System.out.println("pass");
+	   //   System.out.println(url);
+	   }	      
+	}
+
 
    }
-   
+
 
 
 }
